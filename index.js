eaf7aa0d93d9917f436e2a8a6416c1ded5a9533e
AnhKiet1010 <letrananhkiet1010@gmail.com>
2021-05-31 21:56:00 +0700

    change port

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ cron2.start();
 
 app.use(express.static("public"));
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
